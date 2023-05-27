74348f34c9c38a392b3ee012301013aa75ca7e82
mcnoble <idowufestustemiloluwa@gmail.com>
2023-05-28 00:45:05 +0100

    feat: updates :octocat:

--- layouts/PostSingle.js
+++ layouts/PostSingle.js
@@ -1,8 +1,6 @@
 import Share from "@components/Share";
 import dateFormat from "@lib/utils/dateFormat";
-import similerItems from "@lib/utils/similarItems";
 import { humanize, markdownify, slugify } from "@lib/utils/textConverter";
-import SimilarPosts from "@partials/SimilarPosts";
 import shortcodes from "@shortcodes/all";
 import { MDXRemote } from "next-mdx-remote";
 import Image from "next/image";
@@ -10,9 +8,8 @@ import Link from "next/link";
 
 const PostSingle = ({ post, posts, slug }) => {
   const { frontmatter, content, mdxContent } = post;
-  let { description, title, date, image, categories, tags } = frontmatter;
+  let { description, title, date, image } = frontmatter;
   description = description ? description : content.slice(0, 120);
-  const similarPosts = similerItems(post, posts, slug);
 
   return (
     <>
@@ -22,20 +19,6 @@ const PostSingle = ({ post, posts, slug }) => {
             {markdownify(title, "h1", "h2")}
             <ul className="mt-4 mb-8 flex flex-wrap items-center justify-center space-x-3 text-text">
               <li>{dateFormat(date)}</li>
-              <li>
-                <ul>
-                  {categories.map((category, i) => (
-                    <li className="inline-block" key={`category-${i}`}>
-                      <Link
-                        href={`/categories/${slugify(category)}`}
-                        className="mr-3 hover:text-primary"
-                      >
-                        &#9635; {humanize(category)}
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </li>
             </ul>
             {image && (
               <Image
@@ -59,12 +42,6 @@ const PostSingle = ({ post, posts, slug }) => {
               />
             </div>
           </article>
-        </div>
-      </section>
-      <section className="section">
-        <div className="container">
-          <h2 className="mb-8 text-center">Similar Posts</h2>
-          <SimilarPosts posts={similarPosts.slice(0, 3)} />
         </div>
       </section>
     </>
--- layouts/partials/Posts.js
+++ layouts/partials/Posts.js
@@ -1,26 +1,23 @@
 import config from "@config/config.json";
 import dateFormat from "@lib/utils/dateFormat";
-import { humanize, slugify } from "@lib/utils/textConverter";
 import Image from "next/image";
 import Link from "next/link";
 
 const Posts = ({ posts, className }) => {
   const { summary_length } = config.settings;
   return (
-    <div className={`row space-y-16 ${className}`}>
-      {posts.map((post, i) => (
+    <div className={`row ${className}`}>
+      {posts.map((post) => (
         <div
-          key={`key-${i}`}
-          className={i === 0 ? "col-12" : "col-12 sm:col-6"}
+          className="col-12 sm:col-6"
         >
           {post.frontmatter.image && (
             <Image
               className="rounded-lg"
               src={post.frontmatter.image}
               alt={post.frontmatter.title}
-              width={i === 0 ? "925" : "445"}
-              height={i === 0 ? "475" : "230"}
-              // priority={i === 0 ? true : false}
+              width={445}
+              height={230}
             />
           )}
           <ul className="mt-4 mb-4 flex flex-wrap items-center space-x-3 text-text">
